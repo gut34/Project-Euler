be43be35f0c4633e43b17c4029c04219f236c021
MrElectricOps <fogolax13@gmail.com>
2022-07-29 11:46:42 -0600

    updated is_prime formulas

--- Project Euler/Question 7.py
+++ Project Euler/Question 7.py
@@ -10,7 +10,7 @@ By listing the first six prime numbers: 2, 3, 5, 7, 11, and 13, we can see that 
 What is the 10 001st prime number?
 """
 def isprime(x):
-    for i in range(2,x-1):
+    for i in range(2,x**0.5 + 1):
         if x % i == 0:
             return False
     return True
